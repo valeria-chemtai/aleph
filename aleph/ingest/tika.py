e004aa16dce5b743152f8a5d6fcd541ae8614c32
Mark Ng'ang'a <maqnganga@gmail.com>
2017-04-22 13:57:40 +0300

    Change those headers again
     * Last commit broke ingesting completely

--- aleph/ingest/tika.py
+++ aleph/ingest/tika.py
@@ -6,7 +6,7 @@ import requests
 
 def extract_pdf(path, languages=None):
     with open(path, 'rb') as f:
-        headers = {'accept': 'application/pdf', 'content-type': 'text/html'}
+        headers = {'accept': 'text/html', 'content-type': 'application/pdf'}
         tika_url = urljoin(get_config("TIKA_URI"), '/tika')
         r = requests.put(tika_url, data=f, headers=headers)
         r.raise_for_status()
